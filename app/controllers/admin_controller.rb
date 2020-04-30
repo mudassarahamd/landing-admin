d8fa9cab0723e0a14a90a4075d4bdf72609a7f97
Mudassar <mudassarahmadofficial@gmail.com>
2020-04-30 18:26:05 +0500

    admin pannel

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,2 +1,6 @@
 class AdminController < ApplicationController
+
+
+  def tables
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+
   root 'landing#index'
+  get 'admin/index'
+  get 'sign/sign_in'
+  get 'sign/sign_up'
+  get 'admin/tables'
+  get 'admin/404_page'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
